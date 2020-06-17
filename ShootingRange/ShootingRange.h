07601fa685f5c1768af457768e32f3ef977d0856
PiotrSurowiec21 <59863230+PiotrSurowiec21@users.noreply.github.com>
2020-06-17 20:34:44 +0200

    17.06

--- ShootingRange/ShootingRange.h
+++ ShootingRange/ShootingRange.h
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <vector>
 #include <iostream>
+#include <cmath>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
@@ -20,37 +21,61 @@ public:
 class target: public obiect{
 protected:
     double DisplayTime;
+    sf::FloatRect bounds;
     sf::Vector2f position;
-    float speedPerSec;
+    float speedPerSec=100, maxSpeed=200;
     int scale;
 public:
-    target(sf::Texture texture, sf::Vector2f position, int scale, int move): obiect(position){
+    target(sf::Texture & texture, sf::Vector2f position, int Scale): obiect(position){
         setTexture(texture);
-        switch (scale) {
+        scale=Scale;
+        switch (Scale) {
         case 1: setScale(1,1);
             break;
         case 2: setScale(2,2);
             break;
         case 3: setScale(3,3);
+            break;
         }
-
-    }
-    int hit(sf::Vector2f gunpointPos){
-
+        bounds=getGlobalBounds();
     }
 
-    void animate(){
-
+    int hit(sf::Vector2f GPPos){
+        sf::Vector2f center=sf::Vector2f(bounds.left+(bounds.width/2), bounds.top+(bounds.height/2));
+        float r1 = 21*scale, r2 = 10*scale;
+        int points=0;
+        if(GPPos.x>=bounds.left && GPPos.x<=(bounds.left+bounds.width) &&
+                GPPos.y>=bounds.top && GPPos.y<=bounds.top+bounds.height){
+            points++;
+            //std::cout<<powf(GPPos.x-center.x, 2)<<"      "<<((r1*r1)-powf(GPPos.y-center.y,2))<<"      "<< center.x<<"      "<< center.y<<"      "<<r1*r1 <<std::endl;
+           if(powf(GPPos.x-center.x, 2)<=((r1*r1)-powf(GPPos.y-center.y,2))){
+                points++;
+                if(powf(GPPos.x-center.x, 2)<=((r2*r2)-powf(GPPos.y-center.y,2))){
+                    points++;
+                }
+            }
+        }
+        return (points*10)/scale;
     }
 
-
-
+    void animate(int round, float fTime){
+        if(round>10){
+            if(bounds.left<(150/scale)){
+                speedPerSec=fabs(speedPerSec);
+            }
+            else if (bounds.left+bounds.width>1600-(150/scale)) {
+                speedPerSec=-(fabs(speedPerSec));
+            }
+            move(speedPerSec*fTime, 0);
+            bounds=getGlobalBounds();
+        }
+    }
 };
 
 class player: public obiect{
 protected:
-    int points;
-    int hp;
+    int score=0;
+    int hp=5;
 public:
     player(sf::Texture & texture,sf::Vector2f position, sf::Vector2f scale) : obiect(position){
         setTexture(texture);
@@ -60,22 +85,36 @@ public:
     sf::Vector2f checkIfHit(){
         sf::FloatRect bounds= getGlobalBounds();
         sf::Vector2f centerPos(bounds.left+(bounds.width/2), bounds.top+(bounds.height/2));
-        std::cout<<centerPos.x<<"   "<<centerPos.y<<std::endl;
-
+        //std::cout<<centerPos.x<<"   "<<centerPos.y<<std::endl;
+        return centerPos;
     }
 
     void move(sf::Vector2f Recoil, sf::RenderWindow & window){
         sf::Vector2f mousePos= window.mapPixelToCoords(sf::Mouse::getPosition(window))+Recoil;
         setPosition(mousePos);
     }
+    void modifyStats(int points){
+        if(points==0){
+            hp--;
+        }
+        else {
+            score+=points;
+
+        }
+        std::cout<<"hp:"<<hp<<"      score:"<<score<<std::endl;
+    }
 };
 
 class bulletMarks: public obiect{
 public:
-    bulletMarks(sf::Vector2f point, sf::Texture& texture, sf::Vector2f scale):obiect(point){
+    bulletMarks(sf::Vector2f point, sf::Texture & texture, sf::Vector2f scale):obiect(point){
         setTexture(texture);
         setScale(scale);
     }
+};
+
+class interface{
+
 };
 
 #endif // SHOOTINGRANGE_H
--- ShootingRange/main.cpp
+++ ShootingRange/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     srand( time( NULL ) );
 
-
+    //1.OKNO I TLO
     sf::RenderWindow window(sf::VideoMode(1600, 800), "My window");
     window.setMouseCursorVisible(false);
 
@@ -21,7 +21,7 @@ int main()
     sf::Sprite background;
     background.setTexture(BackgroundTex);
 
-
+    //2.CELOWNIK
     sf::Texture GunPointTex;
     if(!GunPointTex.loadFromFile("GunPoint.png")){
         std::cout<<"texture loading fail"<<std::endl;
@@ -31,20 +31,25 @@ int main()
     GunPoint.setTexture(GunPointTex);
     GunPoint.setScale(0.05, 0.05);
 
-    sf::Texture BulletMarkTex;
+    //3.TARCZE
+    sf::Texture TargetTex;
+    if(!TargetTex.loadFromFile("Target.png")){
+        std::cout<<"texture loading fail"<<std::endl;
+    }
+
+    target Target(TargetTex, sf::Vector2f(300,300), 3);
+
+    //4.PRZESTRZELINY
+    /*sf::Texture BulletMarkTex;
     if(!BulletMarkTex.loadFromFile("bulletMark.png")){
         std::cout<<"texture loading fail"<<std::endl;
     }
-
-    sf::Texture TargetTex;
-    if(!TargetTex.loadFromFile("Target2.png")){
-        std::cout<<"texture loading fail"<<std::endl;
-    }
+    std::vector<obiect*> marks;*/
     sf::Vector2f recoil;
-    std::vector<obiect*> marks;
+    sf::Clock clock;
     while (window.isOpen()) {
         sf::Event event;
-
+        sf::Time frameTime = clock.restart();
 
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
@@ -54,7 +59,8 @@ int main()
 
                     //std::cout<<"click"<<std::endl;
                     recoil=sf::Vector2f((std::rand()%20)-20,(std::rand()%20)-10);
-                    marks.emplace_back(new bulletMarks(Player.checkIfHit(), BulletMarkTex, sf::Vector2f(0.1, 0.1)));
+                   // marks.emplace_back(new bulletMarks(Player.checkIfHit(), BulletMarkTex, sf::Vector2f(0.1, 0.1)));
+                    Player.modifyStats(Target.hit(Player.checkIfHit()));
                 }
             }
         }
@@ -63,9 +69,11 @@ int main()
 
         window.draw(background);
 
-        for(auto& o : marks){
-            window.draw(*o);
-        }
+      //  for(auto& o : marks){
+       //     window.draw(*o);
+       // }
+
+        window.draw(Target);
 
         window.draw(Player);
 
